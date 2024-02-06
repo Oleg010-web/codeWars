b1b1414f092ae69eb82d1fc95814c256c29c9c04
Oleg010-web <nesteroff_oleg@mail.ru>
2024-02-07 00:04:30 +0300

    Continue to do tasks on codewars

--- 04.js
+++ 04.js
@@ -29,3 +29,53 @@ function removeEveryOther(arr){
     })
   return arrIndexCounter 
 }
+//5
+const stringToNumber = function(str){
+  return Number(str);
+}
+//6
+function basicOp(operation, value1, value2){
+  if (operation === "+"){
+    return value1 + value2
+  }else if (operation === "-"){
+     return value1 - value2
+  }else if (operation === "*"){
+    return value1 * value2
+  }else if (operation === "/"){
+    return value1 / value2
+  }else {
+    return 0
+  }
+}
+//7
+function countBy(x, n) {
+  let z = [];
+   let multipleNum = x * n
+   while (multipleNum >= x){
+     z.push(multipleNum);
+     multipleNum = multipleNum - x
+   } 
+  z.reverse();
+  return z;
+}
+//8
+function sumMix(x){
+  let sumArraysNum= 0;
+  x.forEach( (element) => {
+    typeof element === "string" ? element = Number(element) : element
+    sumArraysNum += element;
+  })
+  return sumArraysNum;
+}
+//9
+function getSize(width, height, depth){
+  let arr = [];
+  let area = 2 * (width * depth + width * height + height * depth);
+  let volume = width * height * depth;
+  arr.push(area,volume)
+  return arr;
+}
+//10
+const isReallyNaN = (val) => {
+  return val !== val ? true : false
+};
